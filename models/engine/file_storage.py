55fb7abc6f7a8a4973f059b594284d363b503e1f
waltertaya <126944679+waltertaya@users.noreply.github.com>
2024-04-02 01:18:37 +0300

    Updated: Task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,24 +71,13 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        """
-        Returns the object based on the class name and its ID, or None if not
-        found
-        """
-        key = "{}.{}".format(cls, id)
-        if key in self.__objects.keys():
-            return self.__objects[key]
+        """retrieve one object"""
+        all_objs = self.all(cls)
+        for obj in all_objs.values():
+            if obj.id == id:
+                return obj
         return None
 
     def count(self, cls=None):
-        """
-        Returns the number of objects in storage matching the given class name.
-        If no name is passed, returns the count of all objects in storage.
-        """
-        if cls:
-            counter = 0
-            for obj in self.__objects.values():
-                if obj.__class__.__name__ == cls:
-                    counter += 1
-            return counter
-        return len(self.__objects)
+        """count the number of objects in storage"""
+        return len(self.all(cls))
